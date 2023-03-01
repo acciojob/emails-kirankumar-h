50a2aa58b18e7199fe69db40dd8111d0ca3a1366
kirankumar-h <kirankumarak1630@gmail.com>
2023-03-01 14:48:43 +0530

    first commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,11 +19,54 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
+
+            if(oldPassword.equals(password)){
+                if(isValid(newPassword)){
+                    System.out.println("password changed successfully");
+                    this.password = newPassword;
+                }
+                else{
+                    System.out.println("The password is not valid");
+                }
+            }
+            else{
+                System.out.println("The password does not match the current password");
+            }
+
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+    }
+    private boolean isValid(String password){
+        boolean capitalLetter = false;
+        boolean smallLetter = false;
+        boolean digit = false;
+        boolean specialCharacter = false;
+
+        if(password.length() < 8){
+            return false;
+        }
+        for(int i=0;i<password.length();i++){
+            char ch = password.charAt(i);
+
+            if(ch>='A' && ch<='Z'){
+                capitalLetter = true;
+            }
+            else if(ch>='a' && ch<='z'){
+                smallLetter = true;
+            }
+            else if(ch>='0' && ch<='9'){
+                digit = true;
+            }
+            else
+                specialCharacter = true;
+        }
+
+        if(capitalLetter && smallLetter && digit && specialCharacter)
+               return true;
+        return false;
     }
 }
